3f73cd250dd2305b6736eb711b5123e27667ded1
maxq1seg <maximb00131516@gmail.com>
2021-09-15 15:10:36 +0300

    added boilerpalte for matrial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
-import "../styles/global.css"
+// import "../styles/global.css";
 import type { AppProps } from "next/app";
+import React from "react";
+// import { Head } from "next/document";
+import { ThemeProvider } from "styled-components";
+import { CssBaseline } from "@material-ui/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+      {/* </ThemeProvider> */}
+    </>
+  );
 }
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,32 +1,36 @@
-/* eslint no-use-before-define: 0 */ 
+import { ServerStyleSheets } from "@material-ui/styles";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import React from "react";
 
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
-
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet();
+class MyDocument extends Document {
+  static async getInitialProps(ctx:DocumentContext) {
+    const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
+  
+    ctx.renderPage = () => originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  
+    const initialProps = await Document.getInitialProps(ctx);
+  
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    };
+  }
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
+
+export default MyDocument;
